d8f9e7fce22988e8d2fc60319dd1de14f255f739
joana de souza <joanasouzasilva.2008@gmail.com>
2025-10-07 16:29:31 -0300

    criando tabela

---
+++ back-end/funcao.py
@@ -0,0 +1,23 @@
+from conexao import conectar
+
+def criar_tabela():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS filmes (
+                id SERIAL PRIMARY KEY,
+                titulo TEXT NOT NULL,
+                genero TEXT NOT NULL,
+                ano INTEGER NOT NULL,
+                avaliacao REAL
+                )
+            """)
+            conexao.commit()
+        except Exception as erro:
+            print(f"erro ao criar a tabela {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+criar_tabela()
